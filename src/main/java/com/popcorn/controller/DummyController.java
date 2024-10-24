6aa34d646fd1972024fa58b51086cf7711ce8284
Ramakrishna Janapureddy <developer.raakhi005@gmail.com>
2024-10-24 18:48:23 +0530

    added

--- src/main/java/com/popcorn/controller/DummyController.java
+++ src/main/java/com/popcorn/controller/DummyController.java
@@ -8,10 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/dummy-client")
@@ -29,5 +26,12 @@ public class DummyController {
         log.info("DummyController::update {}", request);
         return ResponseEntity.status(HttpStatus.ACCEPTED)
                 .body(dummyService.update(request));
+    }
+
+    @GetMapping(path = "/get", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<ComplexResponseObject> get() {
+        log.info("DummyController::get");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(dummyService.get());
     }
 }
--- src/main/java/com/popcorn/service/DummyService.java
+++ src/main/java/com/popcorn/service/DummyService.java
@@ -30,4 +30,22 @@ public class DummyService {
                 .build();
         return response;
     }
+
+    public ComplexResponseObject get() {
+        log.info("DummyService::get");
+        return ComplexResponseObject.builder()
+                .age((byte) 25)
+                .salary((short) 5000)
+                .married(true)
+                .gender('M')
+                .count(100)
+                .height(5.9f)
+                .serialNumber(123456789L)
+                .amount(2500.75)
+                .name("John Doe")
+                .uuid(UUID.fromString("fb427153-cdb0-471f-aa39-59f0fb458d45"))
+                .revenue(new BigDecimal("100000.50"))
+                .additionalProperties(Map.of())
+                .build();
+    }
 }
